82436ae41eb128eb948d4bdfc9e9156d4172fcd2
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-08 01:31:10 +0100

    Install django-rest-framework toolkit

--- historicalplaces/places/models.py
+++ historicalplaces/places/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 
 # مخطط بيانات للمكان التاريخي
+from django.db.models import DO_NOTHING
+
+
 class Place(models.Model):
     name = models.CharField(max_length=256)  # هذا الحقل يحفظ اسم المكان التاريخي
     description = models.TextField()  # هذا الحقل يحفظ تفاصيل المكان التاريخي
@@ -16,7 +19,7 @@ class Place(models.Model):
 class Rate(models.Model):
     rate = models.IntegerField(default=0)  # يستخدم هذا الحقل لحفظ التقييم
     review = models.CharField(max_length=300)  # يستخدم هذا الحقل لحفظ التعليقات
-    place = models.ForeignKey('Place')  # هذا الحقل يقوم بربط المكان التاريخي بالتعليق
+    place = models.ForeignKey('Place', on_delete=DO_NOTHING)  # هذا الحقل يقوم بربط المكان التاريخي بالتعليق
 
     def __str__(self):
         return '{} - {}'.format(self.place, self.rate)
