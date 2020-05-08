f4d58375fd4ccc4a6e40388fdb997e14b5224af0
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-08 19:48:07 +0100

    Create post method to add new data

--- historicalplaces/places/views.py
+++ historicalplaces/places/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -9,10 +10,19 @@ from places.serializers import PlaceSerializer
 
 
 class PlacesList(APIView):
+    # Get all places
     def get(self, request, format=None):
         # get all places from database
-        places = Place.object.all()
+        places = Place.objects.all()
         # many=True means we gonna send a list of data to serializer
         serializer = PlaceSerializer(places, many=True)
         # return response data as json
         return Response(serializer.data)
+
+    # Create a new place
+    def post(self, request, format=None):
+        # send data from request to serializer
+        serializer = PlaceSerializer(data=request.data)
+        serializer.save() # save data
+        # return response as json with status 201(created)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
