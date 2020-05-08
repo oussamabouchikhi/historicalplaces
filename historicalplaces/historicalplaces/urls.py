0a5ab7e6428eb774340ec9df87561a47a59182fb
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-08 17:54:30 +0100

    include django-rest-framework

--- historicalplaces/historicalplaces/urls.py
+++ historicalplaces/historicalplaces/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api-auth/', include('rest_framework.urls', namespace="rest_framework")),
 ]
