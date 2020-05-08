e10f022d9e21cc71e4d0b35ed771e2dffcf66cd6
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-08 19:14:12 +0100

    Create PlaceList APIView

--- historicalplaces/places/views.py
+++ historicalplaces/places/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from places.models import Place
+from places.serializers import PlaceSerializer
+
+
+class PlacesList(APIView):
+    def get(self, request, format=None):
+        # get all places from database
+        places = Place.object.all()
+        # many=True means we gonna send a list of data to serializer
+        serializer = PlaceSerializer(places, many=True)
+        # return response data as json
+        return Response(serializer.data)
