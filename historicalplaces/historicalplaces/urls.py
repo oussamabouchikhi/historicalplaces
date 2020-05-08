81c5c2483efc2ba67ccaf933d56dacaa74184649
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-08 19:22:26 +0100

    Add places app api urls to base urls

--- historicalplaces/historicalplaces/urls.py
+++ historicalplaces/historicalplaces/urls.py
@@ -20,4 +20,5 @@ from django.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace="rest_framework")),
+    url(r'^api/v1/', include('places.urls', namespace="places")),
 ]
