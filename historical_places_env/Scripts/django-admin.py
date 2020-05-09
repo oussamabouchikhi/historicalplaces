abc79f4eaf2ca13c1255f7c39c5f9519099c4a16
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-09 19:07:21 +0100

    Install dependencies

---
+++ historical_places_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lenovo\desktop\contrib\historicalplaces\historical_places_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
