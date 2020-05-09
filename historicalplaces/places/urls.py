7193a92cf9865bfe512efd2623ab8ca941bc2902
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-09 18:28:14 +0100

    Create Generic Views

--- historicalplaces/places/urls.py
+++ historicalplaces/places/urls.py
@@ -1,6 +1,8 @@
 from  django.conf.urls import url, include
-from places.views import PlacesList
+from places.views import PlacesList, RatesList
 
 urlpatterns = [
     url(r'^places/$', PlacesList.as_view(), name='places_list'),
+    url(r'^rates/$', RatesList.as_view(), name='rates_list'),
+    url(r'^place/(?P<pk>[0-9]+)/$', RatesList.as_view(), name='place_detail'),
 ]
--- historicalplaces/places/views.py
+++ historicalplaces/places/views.py
@@ -1,28 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-from places.models import Place
-from places.serializers import PlaceSerializer
+from places.models import Place, Rate
+from places.serializers import PlaceSerializer, RateSerializer
 
 
-class PlacesList(APIView):
-    # Get all places
-    def get(self, request, format=None):
-        # get all places from database
-        places = Place.objects.all()
-        # many=True means we gonna send a list of data to serializer
-        serializer = PlaceSerializer(places, many=True)
-        # return response data as json
-        return Response(serializer.data)
+class PlacesList(ListCreateAPIView):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
 
-    # Create a new place
-    def post(self, request, format=None):
-        # send data from request to serializer
-        serializer = PlaceSerializer(data=request.data)
-        serializer.save() # save data
-        # return response as json with status 201(created)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class RatesList(ListCreateAPIView):
+    queryset = Rate.objects.all()
+    serializer_class = RateSerializer
+
+
+class PlaceDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Rate.objects.all()
+    serializer_class = PlaceSerializer
