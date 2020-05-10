db4a01d898a07550f390f7e3c767ef2e7b8d6f20
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-10 18:56:59 +0100

    Add auth token app

--- historicalplaces/historicalplaces/settings.py
+++ historicalplaces/historicalplaces/settings.py
@@ -110,6 +110,9 @@ REST_FRAMEWORK = {
     },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    'DEFAULT_AUTHENTICATION_CLASSES': {
+        'rest_framework.authentication.TokenAuthentication',
+    }
 }
 
 # Internationalization
