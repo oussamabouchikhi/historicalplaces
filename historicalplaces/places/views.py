a758e2f59e27d096fd160b9b8eb6514422b31739
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-11 18:31:45 +0100

    Make DjangoModelPermissions to places & rates

--- historicalplaces/places/views.py
+++ historicalplaces/places/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import permissions
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.viewsets import ModelViewSet
 
@@ -9,11 +10,14 @@ from places.serializers import PlaceSerializer, RateSerializer
 
 
 class PlacesViewSet(ModelViewSet):
+    # When adding permission_classes property default permissions from settings.py will be ignored
+    permission_classes = (permissions.DjangoModelPermissionsOrAnonReadOnly,)
     queryset = Place.objects.all()
     serializer_class = PlaceSerializer
 
 
 class RatesViewSet(ModelViewSet):
+    permission_classes = (permissions.DjangoModelPermissions,)
     queryset = Rate.objects.all()
     serializer_class = RateSerializer
 
