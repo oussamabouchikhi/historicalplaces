b7363d339bc85923cd9ebe3635ea006caee5ecce
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-12 18:17:53 +0100

    Throttle requests to 10/day to authenticated users

--- historicalplaces/historicalplaces/settings.py
+++ historicalplaces/historicalplaces/settings.py
@@ -115,9 +115,11 @@ REST_FRAMEWORK = {
     },
     'DEFAULT_THROTTLE_CLASSES': {
         'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
     },
     'DEFAULT_THROTTLE_RATES': {
-        'anon': '5/day'
+        'anon': '5/day',
+        'user': '10/day',
     }
 }
 
